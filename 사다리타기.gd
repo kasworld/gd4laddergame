188234690432cefc04b896d9c79fa402fb904ea3
SeukWon Kang <kasworld@gmail.com>
2024-12-18 13:53:13 +0900

    shift 정리

--- 사다리타기.gd
+++ 사다리타기.gd
@@ -91,7 +91,7 @@ func 사다리문제그리기() -> void:
 				var 가로줄 = Line2D.new()
 				가로줄.default_color = Color.GREEN
 				가로줄.width = 간격.y/10
-				가로줄.points = [가로줄위치(x,y,0), 가로줄위치(x+1,y,0)]
+				가로줄.points = [가로줄위치(x,y), 가로줄위치(x+1,y)]
 				사다리문제.add_child(가로줄)
 
 	사다리문제.visible = true
@@ -103,6 +103,7 @@ func 사다리풀이그리기() -> void:
 	var n = 참가자수.get_value()
 	var 간격 = 사다리간격()
 
+	var shift = Vector2(0,2 * 간격.y/10)
 	for y in n*4:
 		for x in n+1:
 
@@ -113,20 +114,18 @@ func 사다리풀이그리기() -> void:
 				# 첫 세로줄 그리기
 			참가번호 = x % n
 			var 세로줄 = 화살표.instantiate()
-			세로줄.init_2_point(세로줄위치(x,y), 세로줄위치(x,y+1),참가자색[참가번호],간격.x/10/10)
+			세로줄.init_2_point(세로줄위치(x,y), 세로줄위치(x,y+1)-shift,참가자색[참가번호],간격.x/10/10)
 			사다리풀이.add_child(세로줄)
 
 			var 가로줄 = 화살표.instantiate()
 			가로줄.init_wing(0.1)
-			var shift = -2 * 간격.y/10
-			가로줄.init_2_point(가로줄위치(x,y,shift), 가로줄위치(x+1,y,shift),참가자색[참가번호],간격.y/10)
+			가로줄.init_2_point(가로줄위치(x,y)-shift, 가로줄위치(x+1,y)-shift, 참가자색[참가번호],간격.y/10)
 			사다리풀이.add_child(가로줄)
 
 			참가번호 = 사다리풀이자료[x%n][y][1]
 			가로줄 = 화살표.instantiate()
 			가로줄.init_wing(0.1)
-			shift = 2 * 간격.y/10
-			가로줄.init_2_point(가로줄위치(x+1,y,shift), 가로줄위치(x,y,shift),참가자색[참가번호],간격.y/10)
+			가로줄.init_2_point(가로줄위치(x+1,y)+shift, 가로줄위치(x,y)+shift,참가자색[참가번호],간격.y/10)
 			사다리풀이.add_child(가로줄)
 
 	사다리문제.visible = false
@@ -140,17 +139,9 @@ func 세로줄위치(x :int, y :int)->Vector2:
 	var 간격 = 사다리간격()
 	return Vector2(x * 간격.x +간격.x/2, y * 간격.y)
 
-func 가로줄위치(x :int, y :int, shift :int)->Vector2:
+func 가로줄위치(x :int, y :int)->Vector2:
 	var 간격 = 사다리간격()
-	return Vector2(x * 간격.x -간격.x/2, y * 간격.y + 간격.y/2 + shift)
-
-func 세로줄만들기(x :int, y:int, co :Color)->Line2D:
-	var 간격 = 사다리간격()
-	var 세로줄 = Line2D.new()
-	세로줄.default_color = co
-	세로줄.width = 간격.x/10
-	세로줄.points = [ 세로줄위치(x,y), 세로줄위치(x,y+1)]
-	return 세로줄
+	return Vector2(x * 간격.x -간격.x/2, y * 간격.y + 간격.y/2 )
 
 func _on_참가자수_value_changed(_idx: int) -> void:
 	참가자수변경()
