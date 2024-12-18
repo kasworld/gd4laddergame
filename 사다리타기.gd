6b29ca0a1b0b9e942398297848848ae56f1b422e
SeukWon Kang <kasworld@gmail.com>
2024-12-18 13:08:51 +0900

    arrow  bug fix

--- 사다리타기.gd
+++ 사다리타기.gd
@@ -105,23 +105,29 @@ func 사다리풀이그리기() -> void:
 
 	for y in n*4:
 		for x in n+1:
+
 			var 참가번호 = 사다리풀이자료[x%n][y][0]
 			if  참가번호 < 0:
 				continue
+			#if y == 0:
+				# 첫 세로줄 그리기
+			참가번호 = x % n
+			var 세로줄 = 화살표.instantiate()
+			세로줄.init_2_point(세로줄위치(x,y), 세로줄위치(x,y+1),참가자색[참가번호],간격.x/10/10)
+			사다리풀이.add_child(세로줄)
+
 			var 가로줄 = 화살표.instantiate()
 			var shift = -2 * 간격.y/10
 			가로줄.init_2_point(가로줄위치(x,y,shift), 가로줄위치(x+1,y,shift),참가자색[참가번호],간격.y/10)
-			#var 가로줄 = 가로줄만들기(x,y, 참가자색[참가번호], shift *i -shift/2)
 			사다리풀이.add_child(가로줄)
 
 			참가번호 = 사다리풀이자료[x%n][y][1]
 			가로줄 = 화살표.instantiate()
 			shift = 2 * 간격.y/10
 			가로줄.init_2_point(가로줄위치(x+1,y,shift), 가로줄위치(x,y,shift),참가자색[참가번호],간격.y/10)
-			#var 가로줄 = 가로줄만들기(x,y, 참가자색[참가번호], shift *i -shift/2)
 			사다리풀이.add_child(가로줄)
 
-	#사다리문제.visible = false
+	사다리문제.visible = false
 	사다리풀이.visible = true
 
 func 사다리간격() -> Vector2:
@@ -143,14 +149,6 @@ func 세로줄만들기(x :int, y:int, co :Color)->Line2D:
 	세로줄.width = 간격.x/10
 	세로줄.points = [ 세로줄위치(x,y), 세로줄위치(x,y+1)]
 	return 세로줄
-
-func 가로줄만들기(x :int, y:int, co :Color, shift :int) -> Line2D:
-	var 간격 = 사다리간격()
-	var 가로줄 = Line2D.new()
-	가로줄.default_color = co
-	가로줄.width = 간격.y/10
-	가로줄.points = [ 가로줄위치(x,y,shift), 가로줄위치(x+1,y,shift)]
-	return 가로줄
 
 func _on_참가자수_value_changed(_idx: int) -> void:
 	참가자수변경()
